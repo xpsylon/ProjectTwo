f7baede6ae27aa959ef1e21b4600340139a8945f
xpsylon <xxxpsylon@gmail.com>
2023-03-13 20:06:33 +0100

    little update

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='indice'),
+    path('<int:question_id>/', views.detail, name='detalle'),
+    path('<int:question_id>/results/', views.results, name='resultados'),
+    path('<int:question_id>/vote/', views.vote, name='votos'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('<h2>Hola, estas en polls/index</h2>')
 
+def detail(request, question_id):
+    return HttpResponse(f'You are looking at question {question_id}.')
+
+def results(request, question_id):
+    response = 'You are looking at the results of question {question_id}.'
+    return HttpResponse(response.format(question_id=question_id))
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting on question {question_id}.')
+
 
