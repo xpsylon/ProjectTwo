f76b0af5da63f48f2096f499fd17bab7139c5564
xpsylon <xxxpsylon@gmail.com>
2023-03-14 19:11:13 +0100

    partial commit

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:2] #en lista las preguntas por pub_date
+    latest_question_list = Question.objects.order_by('-pub_date')[:3] #en lista las preguntas por pub_date
     output = ', '.join([q.question_text for q in latest_question_list])# iteracion y creacion nueva lista a traves de comprehension list.
     return HttpResponse(output)
 
