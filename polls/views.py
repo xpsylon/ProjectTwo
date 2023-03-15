88bef1355e8d33ca8b196a8006153f8feba5da22
xpsylon <xxxpsylon@gmail.com>
2023-03-15 17:09:19 +0100

    until tutorial 3

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponse
 #from django.template import loader #cuando usamos render no se necesita el loader
 from .models import Question
+from django.http import Http404
 
 """ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:3] #en lista las preguntas por pub_date
     output = ', '.join([q.question_text for q in latest_question_list])# iteracion y creacion nueva lista a traves de comprehension list.
     return HttpResponse(output)
  """
+
 """ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #order list by pub_date descendente(-), slicing 5
@@ -17,14 +19,23 @@ from .models import Question
     }
     return HttpResponse(plantilla.render(context, request))
  """
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:3]
     context = {'lista_ultimas_preguntas':latest_question_list}
     return render(request, 'polls/index.html', context)
 
 
-def detail(request, question_id):
+""" def detail(request, question_id):
     return HttpResponse(f'You are looking at question {question_id}.')
+ """
+
+def detail(request, question_id):
+    try:
+        frage = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Esa pregunta no existe")
+    return render(request, 'polls/detail.html', {'pregunta':frage})#como context va literal de diccionario (manualmente)
 
 def results(request, question_id):
     response = 'You are looking at the results of question {question_id}.'
