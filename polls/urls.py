43457263780f8b2100aac4e4a005dc30cac71130
xpsylon <xxxpsylon@gmail.com>
2023-07-05 17:41:35 +0200

    final p2

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'polls' #nombre de la app para diferenciar los names de los urls distintas apps.
 urlpatterns = [
-    path('', views.index, name='indice'),
-    path('<int:question_id>/', views.detail, name='detalle'),
-    path('<int:question_id>/results/', views.results, name='resultados'),
+    path('', views.IndexView.as_view(), name='indice'),
+    path('<int:pk>/', views.DetailQuestionView.as_view(), name='detalle'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='resultados'),
     path('<int:question_id>/vote/', views.vote, name='votos'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,13 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 #from django.template import loader #cuando usamos render no se necesita el loader
-from .models import Question
-from django.http import Http404
-
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
 """ 
-OLD HARCODED
+OLD DUMMY DATA
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5] #en lista las preguntas por pub_date
     output = ', '.join([q.question_text for q in latest_question_list])# iteracion y creacion nueva lista a traves de comprehension list.
@@ -24,14 +26,24 @@ def index(request):
     return HttpResponse(plantilla.render(context, request))
  """
 
-def index(request):
+"""
+OLD. REPLACED BY GENERIC (CLASS-BASED) VIEW
+ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'lista_ultimas_preguntas':latest_question_list}
     return render(request, 'polls/index.html', context)
+ """
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'lista_ultimas_preguntas'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Question.objects.order_by('-pub_date')[:5]
 
 
 """ 
-OLD HARD CODED
+OLD DUMMY DATA
 def detail(request, question_id):
     return HttpResponse(f'You are looking at question {question_id}.')
  """
@@ -40,22 +52,54 @@ def detail(request, question_id):
 OLD
 def detail(request, question_id):
     try:
-        frage = Question.objects.get(pk=question_id)
+        question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Esa pregunta no existe")
-    return render(request, 'polls/detail.html', {'pregunta':frage})#como context va literal de diccionario (manualmente)
+    return render(request, 'polls/detail.html', {'pregunta':question})#como context va literal de diccionario (manualmente)
  """
 
+""" 
+OLD. REPLACED BY GENERIC (CLASS-BASED) VIEW
 def detail(request, question_id):
-    frage = get_object_or_404(Question, id=question_id)
+    frage = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'pregunta':frage})
+ """
 
+class DetailQuestionView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-
-
-def results(request, question_id):
+"""
+OLD
+DUMMY DATA
+ def results(request, question_id):
     response = 'You are looking at the results of question {question_id}.'
     return HttpResponse(response.format(question_id=question_id))
+ """
+
+
+""" OLD. REPLACED BY GENERIC (CLASS-BASED) VIEW
+def results(request, question_id):
+    frage = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'pregunta': frage}) """
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+""" 
+OLD 
+DUMMY DATA
+def vote(request, question_id):
+    return HttpResponse(f'You are voting on question {question_id}.') """
 
 def vote(request, question_id):
-    return HttpResponse(f'You are voting on question {question_id}.')
+    frage = get_object_or_404(Question, pk=question_id)
+    try:
+        opcion_elegida = frage.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'pregunta':frage, 'error_message': 'No ha seleccionado una opcion'})
+    else:
+        opcion_elegida.votes += 1
+        opcion_elegida.save()
+    return HttpResponseRedirect(reverse('polls:resultados', args=(question_id,)))
