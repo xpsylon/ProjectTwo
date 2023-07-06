156340e5ab9eb24daa9d38748c8088f10a83c63b
xpsylon <xxxpsylon@gmail.com>
2023-07-06 10:58:08 +0200

    practice api CRUD

--- javier/models.py
+++ javier/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class Person(models.Model):
@@ -22,3 +23,30 @@ class Membership(models.Model):
     date_joined = models.DateField()
     invite_reason = models.CharField(max_length=64)
 
+class Blog(models.Model):
+    name = models.CharField(max_length=200) #Blog El Invierno Sovietico (ejemplo)
+    tagline = models.TextField() #Las terribles historias de la invasion nazi a la USSR.
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField(default=date.today)
+    authors = models.ManyToManyField(Author)
+    number_of_comments = models.IntegerField(default=0)
+    number_of_pingbacks = models.IntegerField(default=0)
+    rating = models.IntegerField(default=5)
+
+    def __str__(self) -> str:
+        return self.headline
